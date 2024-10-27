f66700dd08e461d6b508d771ab63e0cb8dabbf87
Francisco Valdes <ecodisonante@gmail.com>
2024-10-27 19:31:45 -0300

    Actualizar usuario con reglas custom
    - Si el usuario logeado es admin, puede modificar cualquier usuario. En caso contrario solo podrá modificar su propio usuario.

--- src/main/java/com/destilado_express/usuarioservice/controller/UsuarioController.java
+++ src/main/java/com/destilado_express/usuarioservice/controller/UsuarioController.java
@@ -5,6 +5,8 @@ import com.destilado_express.usuarioservice.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,37 +55,37 @@ public class UsuarioController {
         }
     }
 
-    // Crear un nuevo usuario
+    // Crear nuevo usuario
     @PostMapping
     public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario) {
         Usuario nuevoUsuario = usuarioService.crearUsuario(usuario);
         return new ResponseEntity<>(nuevoUsuario, HttpStatus.CREATED);
     }
 
-    // // Crear un nuevo usuario
-    // @PostMapping
-    // public ResponseEntity<Usuario> crearUsuario(@RequestBody Usuario usuario, Authentication authentication) {
-    //     String rolUsuarioActual = authentication.getAuthorities().iterator().next().getAuthority();
-        
-    //     // Si el usuario actual es admin, puede asignar rol, de lo contrario asignar CLIENTE por defecto
-    //     if ("ROLE_ADMIN".equals(rolUsuarioActual)) {
-    //         // Admin puede asignar cualquier rol
-    //         return new ResponseEntity<>(usuarioService.crearUsuario(usuario), HttpStatus.CREATED);
-    //     } else {
-    //         // No admin, forzar rol CLIENTE
-    //         usuario.setRol(rolRepository.findByNombre("CLIENTE"));
-    //         return new ResponseEntity<>(usuarioService.crearUsuario(usuario), HttpStatus.CREATED);
-    //     }
-    // }
-
-    // Actualizar un usuario existente
+    // Actualizar usuario
     @PutMapping("/{id}")
-    public ResponseEntity<Usuario> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
+    public ResponseEntity<String> actualizarUsuario(@PathVariable Long id, @RequestBody Usuario usuarioActualizado) {
+
+        // Obtener correo y rol del usuario autenticado
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        boolean isAdmin = authentication.getAuthorities().stream()
+                .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
+
+        // Si no es admin solo puede modificar su propio usuario
+        if (!isAdmin) {
+            Usuario registeredUser = usuarioService.getUsuarioByEmail(username);
+            if (registeredUser.getId() != id) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                        .body("No tienes permiso para actualizar otros usuarios");
+            }
+        }
+
         Usuario usuario = usuarioService.actualizarUsuario(id, usuarioActualizado);
         if (usuario != null) {
-            return new ResponseEntity<>(usuario, HttpStatus.OK);
+            return ResponseEntity.status(HttpStatus.OK).body("Usuario actualizado");
         } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("NO se encontró el usuario con ese ID");
         }
     }
 
